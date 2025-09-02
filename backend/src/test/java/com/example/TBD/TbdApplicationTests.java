443c4041c7144e25e9dd3ec2ef764ad79ce053e8
takuto147 <motherbasis39@gmail.com>
2025-09-02 23:43:43 +0900

    initial

---
+++ backend/src/test/java/com/example/TBD/TbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
